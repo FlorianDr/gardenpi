7a5dc3bcf925068383b6ee8ef7be0c31042e5684
Florian Dreschner <fdreschner@brofit.de>
2015-07-28 22:05:42 +0200

    - Changed sensor data order - Fixed no method error

--- dht.py
+++ dht.py
@@ -4,8 +4,8 @@ import Adafruit_DHT
 sensor = Adafruit_DHT.AM2302
 pin = 4
 
-def read:
+def read():
     while True:
         humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
         if humidity is not None and temperature is not None:
-            return (humidity, temperature)
+            return (temperature, humidity)
