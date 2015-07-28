0603a462d0ea140c00482a0ec7e970842445083e
Maximilian <contact@mxlink.de>
2015-07-28 22:30:27 +0200

    added output

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ import valve
 while True:
 	soil_humid = soil_humidity.read()
 	db.writeSoilHum(soil_humid)
-	db.writeHumAndTemp(dht.read())
-	if (soil_humid < 50)
+	print soil_humid
+	
+	hum_and_temp = dht.read()
+	db.writeHumAndTemp(hum_and_temp)
+	print hum_and_temp
+
+	if (soil_humid < 50):
 		valve.open(17, 5)
+	
 	time.sleep(10)
