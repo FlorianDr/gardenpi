ba4100fc6c8986f203c3a15a94bcd0231d3dc3d8
Maximilian <contact@mxlink.de>
2015-07-28 22:04:57 +0200

    added file for controlling valve

---
+++ valve.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import time
+import RPi.GPIO as GPIO, time, os
+
+def open(seconds, pin):
+	GPIO.setup(pin, GPIO.OUT)
+	GPIO.output(pin, True)
+	time.sleep(seconds)
+	GPIO.output(pin, False)
+	GPIO.cleanup()
